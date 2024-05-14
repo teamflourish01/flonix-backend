fc349420a889220123acdae8b3966f07ae5d25ad
Prince Mendpara <prince.m.7405@gmail.com>
2024-05-14 10:15:35 +0530

    blog created

--- model/BlogModel.js
+++ model/BlogModel.js
@@ -1,18 +1,42 @@
-const mongoose=require("mongoose")
-const options={
-    versionKey: false,
-    timestamps: {
+const mongoose = require("mongoose");
+const options = {
+  versionKey: false,
+  timestamps: {
     createdAt: true,
     updatedAt: "modifiedAt",
+  },
+};
+const BlogSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-}
-const BlogSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+    banner_image: {
+      type: String,
     },
-    banner_image:{
-        type:String
+    first_image: {
+      type: String,
     },
-    
-},options)
+    text1: {
+      type: String,
+    },
+    text2: {
+      type: String,
+    },
+    second_image: {
+      type: String,
+    },
+    text3: {
+      type: String,
+    },
+    third_image: {
+      type: String,
+    },
+  },
+  options
+);
+
+const BlogModel=mongoose.model("Blog",BlogSchema)
+module.exports={BlogModel}
+
